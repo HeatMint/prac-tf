149f56db505725edeb84e59c069d9411a120b7d5
heatmint <heatmint2333@gmail.com>
2025-10-16 17:04:40 +0800

    working user data on ec2

--- tf/main.tf
+++ tf/main.tf
@@ -14,15 +14,5 @@ resource "aws_instance" "web_server" {
 
   key_name = "key-pair-1"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              # Install requirements
-              sudo yum install python -y
-              sudo yum install pip -y
-              pip install flask
-              sudo yum install git -y
-              git clone https://github.com/HeatMint/prac-tf.git
-              pip install gunicorn
-              python ./prac-tf/flask/app.py
-              EOF
+  user_data = file("${path.module}/../scripts/launch_ec2.sh")
 }
