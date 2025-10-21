961baacf9045103155d6fbca634b75c8a549c3db
heatmint <heatmint2333@gmail.com>
2025-10-21 16:00:27 +0800

    change region

--- tf/main.tf
+++ tf/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-052064a798f08f0d3"
+  ami           = "ami-0199d4b5b8b4fde0e"
   instance_type = "t3.micro"
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_instance" "web_server" {
 
   security_groups = [aws_security_group.web_server_1.name]
 
-  key_name = "key-pair-1"
+  key_name = "key-pair-2"
 
   user_data = file("${path.module}/../scripts/launch_ec2.sh")
 }
