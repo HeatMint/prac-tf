0045906b1304dc41e02a502786a92cb84c7b26e3
heatmint <heatmint2333@gmail.com>
2025-10-22 02:16:49 +0800

    force redeploy in apigw

--- tf/main.tf
+++ tf/main.tf
@@ -164,6 +164,14 @@ resource "aws_api_gateway_deployment" "api" {
 
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "prod"
+  # Force new deployment when integration changes
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.path.id,
+      aws_api_gateway_method.proxy.id,
+      aws_api_gateway_integration.lambda.id,
+    ]))
+  }
 }
 
 resource "aws_lambda_permission" "api_gw" {
