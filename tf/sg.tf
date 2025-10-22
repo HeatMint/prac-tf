3f4c4ac84b705f0acc1950931cb95a731b931232
heatmint <heatmint2333@gmail.com>
2025-10-22 16:17:03 +0800

    refractor sg ingress for reuse

--- tf/sg.tf
+++ tf/sg.tf
@@ -1,29 +1,60 @@
+locals {
+  common_ingress = [
+    {
+      description = "SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+    },
+    {
+      description = "ICMP"
+      from_port   = -1
+      to_port     = -1
+      protocol    = "icmp"
+    },
+    {
+      description = "TCP port 5000"
+      from_port   = 5000
+      to_port     = 5000
+      protocol    = "tcp"
+    }
+  ]
+}
+
 resource "aws_security_group" "web_server_1" {
   name        = "allow_ssh_icmp_tcp_5000"
   description = "Allow SSH, ICMP, and TCP port 5000"
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere (adjust for security)
+  dynamic "ingress" {
+    for_each = local.common_ingress
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
-  ingress {
-    description = "ICMP"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow ICMP from anywhere
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  ingress {
-    description = "TCP port 5000"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow TCP port 5000 from anywhere
+resource "aws_security_group" "web_server_1_internal" {
+  name        = "vpc-only-allow_ssh_icmp_tcp_5000"
+  vpc_id      = data.aws_vpc.default.id
+
+  dynamic "ingress" {
+    for_each = local.common_ingress
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = [data.aws_vpc.default.cidr_block]
+    }
   }
 
   egress {
